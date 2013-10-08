1336c647ce8508ad1c81fb3487ea7500f161bf55
David Rouleau <Shabbypenguin@gmail.com>
2013-10-07 22:37:46 -0400

    Hide SElinux error from printing

--- recovery.c
+++ recovery.c
@@ -961,7 +961,7 @@ main(int argc, char **argv) {
 
     if (!sehandle) {
         fprintf(stderr, "Warning: No file_contexts\n");
-        ui_print("Warning:  No file_contexts\n");
+        ui_print("\n");
     }
 
     LOGI("device_recovery_start()\n");
